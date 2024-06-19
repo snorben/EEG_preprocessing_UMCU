a90a83ddbf6e2fad2e649f4db15d08dc75ee6276
snorben <yorbenlodema@proton.me>
2024-06-19 17:17:56 +0200

    initial

---
+++ src/eeg_preprocessing_umcu/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about eeg_preprocessing_umcu."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Yorben Lodema"
+__email__ = "d.y.lodema-2@umcutrecht.nl"
+__version__ = "0.1.0"
